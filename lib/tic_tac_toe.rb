a87c9625ee7b19d623e300c4f7404a1984d85fac
German Cruz <cruzgerman216@yahoo.com>
2020-03-26 06:28:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -84,14 +84,7 @@ end
     end
   end
   def play
-    until over? == true
-      turn
-    end
-    if won?
-      puts "Congratulations #{winner}!"
-    elsif draw?
-      puts "Cat's Game!"
-    end
+  
   end
 end
 game = TicTacToe.new
